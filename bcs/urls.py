2c0fed38e49a45ec37e1116b09b8243174d87d14
João Paulo Zorek <joaopaulozorek@gmail.com>
2020-10-15 16:01:13 -0300

    Add bc_detail.html

--- bcs/urls.py
+++ bcs/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('', views.bc_list, name='bc_list'),
-	#path('')
+	path('bc/<user>', views.bc_detail, name='bc_detail'),
     #path('', views.post_list, name='post_list'),
 ]
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bcs/views.py
+++ bcs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 
@@ -6,3 +6,11 @@ from .models import *
 def bc_list(request):
 	bcs = Bc.objects.all()
 	return render(request, 'bcs/bc_list.html', {'bcs':bcs})
+
+def bc_detail(request, user):
+	usuario = get_object_or_404(Usuario, usu_user__username=user)
+	print(usuario)
+	bc = get_object_or_404(Bc, bc_usuario=usuario.pk)
+	print(bc)
+
+	return render(request,'bcs/bc_detail.html',{'bc':bc})
