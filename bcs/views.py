04573bc576c31c599c91972ceb535f67e62df49c
João Paulo Zorek <joaopaulozorek@gmail.com>
2020-10-20 13:19:55 -0300

    Editando os templates

--- bcs/views.py
+++ bcs/views.py
@@ -5,7 +5,7 @@ from .models import *
 
 def bc_list(request):
 	bcs = Bc.objects.all()
-	return render(request, 'bcs/bc_list.html', {'bcs':bcs})
+	return render(request, 'bcs/bc_list.html', {'bcs':bcs})	
 
 def bc_detail(request, user):
 	usuario = get_object_or_404(Usuario, usu_user__username=user)
